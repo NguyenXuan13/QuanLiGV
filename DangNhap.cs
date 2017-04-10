13fcba55cf3a89c7749afdc40afb1e1c117ad33f
NguyenXuan13 <eragonzappy@gmail.com>
2017-04-10 10:55:08 +0700

    Add files via upload
    da hoan thanh form dang nhap

---
+++ DangNhap.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLGV_HS
+{
+    public partial class frmDangNhap : Form
+    {
+        public frmDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void txtMatKhau_TextChanged(object sender, EventArgs e)
+        {
+            txtMatKhau.UseSystemPasswordChar = true;
+        }
+
+        private void txtMatKhau_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtMatKhau.Clear();
+            txtMatKhau_TextChanged(sender, e);
+            
+        }
+
+        private void txtMatKhau_FontChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTaiKhoan_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTaiKhoan_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtTaiKhoan.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmQuanLiGV fr = new frmQuanLiGV();
+            fr.ShowDialog();
+        }
+    }
+}
